b6f3758d9a43566957c2688fb612e6056a774d24
LorelleW <lorelle.weng@icloud.com>
2024-10-24 16:20:45 +0200

    Update arme.py

--- classes/arme.py
+++ classes/arme.py
@@ -1,16 +1,23 @@
 public class Arme():
-    def __init__(self,type4='poing':str,restriction,attaque=0:int,rarete,etat:str): 
+    def __init__(self,type4='poing':str,restriction,attaque=0:int,rarete,etat:str,prix:str): 
         self.__type4=type4
         self.__restriction=restriction
         self.__attaque=attaque
         self.__rarete=rarete
         self.__etat=etat
+        self.__prix=prix
 
     def get_type4(self):
         return self.__type4
     
     def set_type4(self, type4):
-        self.__nom = type4 
+        self.__type4 = type4 
+
+    def get_prix(self):
+        return self.__prix
+    
+    def set_prix(self, prix):
+        self.__prix = prix
 
     def get_restriction(self):
         return self.__restriction
