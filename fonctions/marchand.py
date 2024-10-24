1f1f4f23513f0c9cfbdb1dcbc06f5d037095a425
LorelleW <lorelle.weng@icloud.com>
2024-10-24 17:34:11 +0200

    Update marchand.py

--- fonctions/marchand.py
+++ fonctions/marchand.py
@@ -3,7 +3,23 @@ from classes.joueur import Joueur
 from classes.inventaire import Inventaire
 from classes.echoppe import Echoppe
 
-def vendre():
-  inventaire_joueur=joueur.get_inventaire(self)
-  
+def vendre(item):
+  inventaire_joueur=Joueur.get_inventaire()
+  x=-1
+  for liste in (inventaire_joueur):
+    x+=1
+    if item in liste:
+      break;
+    else:
+      print("Error404")
+  if x==0:
+    prix=Livre.get.__prix()
+  if x==1:
+    prix=Armure.get.__prix()
+  if x==2:
+    prix=Arme.get.__prix()
+  if x==3:
+    prix=Objet.get.__prix()
+  else:
+    print("Erreur")
   
