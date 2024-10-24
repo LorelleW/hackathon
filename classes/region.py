0392bf576fc889e50a3ceb345920abd7f8eae27a
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 14:00:30 +0200

    Create region.py

---
+++ classes/region.py
@@ -0,0 +1,7 @@
+class region():
+    def __init__(self,typedelieu,topographie,population,position,proprietaire:str):
+        self.__typedelieu=typedelieu
+        self.__topographie=topographie
+        self.__population=population
+        self.__position=position
+        self.__proprietaire=proprietai
