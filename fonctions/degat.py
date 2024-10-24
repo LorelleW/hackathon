ca6e615f60202774611b4debd0ca266d00ef90d0
Floriannee <floriannepan@gmail.com>
2024-10-24 16:47:26 +0200

    Update degat.py

--- fonctions/degat.py
+++ fonctions/degat.py
@@ -3,32 +3,34 @@ from classes.arme import Arme
 from classes.joueur import Joueur
 from classes.creature import Creature
 
-attaque_joueur=Joueur.get_attaque(self)
-attaque_arme_joueur=Arme.get_attaque(Joueur.get_arme(self)) #str nom arme 
 
-attaque_creature=Creature.get_attaque(self)
-attaque_arme_creature=Arme.get_attaque(Creature.get_arme(self))
 
 def degat_joueur():
+  attaque_joueur=Joueur.get_attaque()
+  arme_joueur = Joueur.get_arme()  # Pas besoin de passer 'joueur' ici
+  attaque_arme_joueur = arme_joueur.get_attaque() #str nom arme 
   return (attaque_joueur+attaque_arme_joueur)
 
 def degat_creature():
+  attaque_creature=Creature.get_attaque()
+  arme_creature = Creature.get_arme()  # Pas besoin de passer 'joueur' ici
+  attaque_arme_creature=arme_creature.get_attaque()
   return (attaque_creature+attaque_arme_creature)
 
 def pv_actuel_joueur():
-  pv_joueur=joueur.get_pv(self)
+  pv_joueur=Joueur.get_pv()
   degat_creat=degat_creature()
   pv_joueur -= degat_creat
-  joueur.set_pv(self,pv_joueur)
+  Joueur.set_pv(pv_joueur)
   if (pv_joueur>0):
     return (pv_joueur)
   else:
     print("Tu es mort...")
 
 def pv_actuel_creature():
-  pv_creature=creature.get_pv(self)
+  pv_creature=Creature.get_pv()
   degat_joueur=degat_joueur()
   pv_creature -= degat_joueur
-  creature.set_pv(self,pv_creature)
+  Creature.set_pv(pv_creature)
   if (pv_creature<=0):
     print("Vous avez vaincu le monstre")
