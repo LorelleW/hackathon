982b075b52919bb3c1bc9a65af2ecd4d3d508bf6
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 15:57:21 +0200

    Update competence.py

--- classes/competence.py
+++ classes/competence.py
@@ -4,3 +4,27 @@ public class Competence():
         self.__niveau=niveau
         self.__description=description
         self.__delai=delai
+
+    def get_nom(self):
+        return self.__nom
+
+    def set_nom(self, nom):
+        self.__nom = nom
+
+    def get_niveau(self):
+        return self.__niveau
+
+    def set_niveau(self, niveau):
+        self.__niveau = niveau
+
+    def get_description(self):
+        return self.__description
+
+    def set_description(self, description):
+        self.__description = description
+
+    def get_delai(self):
+        return self.__delai
+
+    def set_delai(self, delai):
+        self.__delai = delai
