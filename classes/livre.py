418b75c110127cc139f8aa1f73b9f0224c78b196
Minetriforce <jonathan.ngo@etu.cyu.fr>
2024-10-24 17:55:41 +0200

    Update livre.py

--- classes/livre.py
+++ classes/livre.py
@@ -1,5 +1,5 @@
 class Livre():
-    def __init__(self,nom:str,type7:str,prix:int,effets:str,rarete,prix): 
+    def __init__(self,nom:str,type7:str,prix:int,effets:str,rarete): 
         self.__nom=nom
         self.__type7=type7
         self.__prix=prix
