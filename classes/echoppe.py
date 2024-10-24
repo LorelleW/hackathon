a11dfd51897ce94fc7039127e1dc3a3f39cfab86
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 16:01:37 +0200

    Update echoppe.py

--- classes/echoppe.py
+++ classes/echoppe.py
@@ -4,4 +4,34 @@ public class Echoppe():
         self.__argent=argent
         self.__batiment=batiment
         self.__type6=type6 
-        self.__proprietaire=proprietaire 
+        self.__proprietaire=proprietaire
+
+    def get_inventaire(self):
+        return self.__inventaire
+
+    def set_inventaire(self, inventaire):
+        self.__inventaire = inventaire
+
+    def get_argent(self):
+        return self.__argent
+
+    def set_argent(self, argent):
+        self.__argent = argent
+
+    def get_batiment(self):
+        return self.__batiment
+
+    def set_batiment(self, batiment):
+        self.__batiment = batiment
+
+    def get_type6(self):
+        return self.__type6
+
+    def set_type6(self, type6):
+        self.__type6 = type6
+
+    def get_proprietaire(self):
+        return self.__proprietaire
+
+    def set_proprietaire(self, proprietaire):
+        self.__proprietaire = proprietaire
