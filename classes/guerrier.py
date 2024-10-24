c3ea06d8efb102df1132f335c9d0f759b684bf70
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 14:07:09 +0200

    Create guerrier.py

---
+++ classes/guerrier.py
@@ -0,0 +1,12 @@
+class guerrier(): 
+    def _init_(self,race,niv,pvmax,pv,attaque,defense,argent,classe,competence,statistique,identite):
+        self.__race = race
+        self.__classe = classe
+        self.__competence = competence
+        self.__niv=niv
+        self.__pvmax=pvmax
+        self.__pv=pv
+        self.__attaque=attaque
+        self.__defense=defense
+        self.__argent=argent
+        self.__identite = identite
