391cf40b7698b70c8dd0254dec7029d2b65dddf4
LorelleW <lorelle.weng@icloud.com>
2024-10-24 15:12:35 +0200

    Update degat.py

--- fonctions/degat.py
+++ fonctions/degat.py
@@ -3,5 +3,14 @@ from classes.arme import *
 from classes.joueur import *
 from classes.creature import *
 
-attaque_arme=arme.get_attaque(self)
 attaque_joueur=joueur.get_attaque(self)
+attaque_arme_joueur=arme.get_attaque(joueur.arme) #str nom arme 
+
+attaque_creature=creature.get_attaque(self)
+attaque_arme_creature=arme.get_attaque(creature.arme)
+
+def degat_joueur():
+  return (attaque_joueur+attaque_arme_joueur)
+
+def degat_creature():
+  return (attaque_creature+attaque_arme_creature)
