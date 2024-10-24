e2e53dc67dc23f211cf9518ba9a9e8d9d31ddc02
Minetriforce <jonathan.ngo@etu.cyu.fr>
2024-10-24 15:56:34 +0200

    Update livre.py

--- classes/livre.py
+++ classes/livre.py
@@ -5,3 +5,39 @@ public class Livre():
         self.__prix=prix
         self.__effets=effets
         self.__rarete=rarete
+
+
+# Getter et Setter pour nom
+    def get_nom(self):
+        return self.__nom
+
+    def set_nom(self, nom):
+        self.__nom = nom
+
+    # Getter et Setter pour type7
+    def get_type7(self):
+        return self.__type7
+
+    def set_type7(self, type7):
+        self.__type7 = type7
+
+    # Getter et Setter pour prix
+    def get_prix(self):
+        return self.__prix
+
+    def set_prix(self, prix):
+        self.__prix = prix
+
+    # Getter et Setter pour effets
+    def get_effets(self):
+        return self.__effets
+
+    def set_effets(self, effets):
+        self.__effets = effets
+
+    # Getter et Setter pour rarete
+    def get_rarete(self):
+        return self.__rarete
+
+    def set_rarete(self, rarete):
+        self.__rarete = rarete
