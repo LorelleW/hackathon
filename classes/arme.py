008118e09a7f231ab9b6c9a2e969857e0c11dc20
Minetriforce <jonathan.ngo@etu.cyu.fr>
2024-10-24 15:28:47 +0200

    Update arme.py

--- classes/arme.py
+++ classes/arme.py
@@ -6,33 +6,33 @@ public class Arme():
         self.__rarete=rarete
         self.__etat=etat
 
-def get_type4(self):
-    return self.__type4
+    def get_type4(self):
+        return self.__type4
     
-def set_type4(self, type4):
+    def set_type4(self, type4):
         self.__nom = type4 
 
-def get_restriction(self):
-    return self.__restriction
+    def get_restriction(self):
+        return self.__restriction
 
-def set_restriction(self, restriction):
-    self.__restriction = restriction
+    def set_restriction(self, restriction):
+        self.__restriction = restriction
 
-def get_attaque(self):
-    return self.__attaque
+    def get_attaque(self):
+        return self.__attaque
 
-def set_attaque(self,attaque):
-    self._attaque = attaque
+    def set_attaque(self,attaque):
+        self._attaque = attaque
 
-def get_rarete(self):
-    return self.__rarete
+    def get_rarete(self):
+        return self.__rarete
 
-def set_rarete(self,rarete):
-    self._rarete = rarete
+    def set_rarete(self,rarete):
+        self._rarete = rarete
 
-def get_etat(self):
+    def get_etat(self):
     return self.__etat
 
-def set_etat(self,etat):
-    self.__etat = etat 
+    def set_etat(self,etat):
+        self.__etat = etat 
     
