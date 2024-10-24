3d8f07acc6e2998c3f96c524044df5439a146cde
Floriannee <floriannepan@gmail.com>
2024-10-24 16:25:27 +0200

    Update marchand.py

--- fonctions/marchand.py
+++ fonctions/marchand.py
@@ -3,4 +3,7 @@ from classes.joueur import Joueur
 from classes.inventaire import Inventaire
 from classes.echoppe import Echoppe
 
-
+def vendre():
+  inventaire_joueur=joueur.get_inventaire(self)
+  
+  
