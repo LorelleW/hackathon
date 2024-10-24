cd4a85306ca231e7d31ffe339bfe5a54110e2517
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 14:21:27 +0200

    Update arme.py

--- classes/arme.py
+++ classes/arme.py
@@ -1,4 +1,4 @@
-public class arme():
+public class Arme():
     def __init__(self,type4:str,restriction,attaque,rarete,etat:str): 
         self.__type4=type4
         self.__restriction=restriction
