4289f1476e4c5d11cebcc24c11b3a1d0a5287f98
2ec3 <eric-shao@hotmail.com>
2024-10-24 13:59:56 +0200

    Update creature.py

--- classes/creature.py
+++ classes/creature.py
@@ -1,1 +1,8 @@
-
+class creature():
+    def __init__(self,nom:str,race:str,identite,classe,competences,statistiques,butin): #butin creer nouvelle class? 
+        self.__nom=nom
+        self.__race=race
+        self.__classe=classe
+        self.__competences=competences
+        self.__statistiques=statistiques
+        self.__butin=butin
