5ee44712af75f7d3bec69c76c614df84faf147a6
2ec3 <eric-shao@hotmail.com>
2024-10-24 21:45:46 +0200

    Update creature.py

--- classes/creature.py
+++ classes/creature.py
@@ -1,10 +1,7 @@
 class Creature():
-    def __init__(self,identite:str,race:str,classe,niv,pvmax,pv,attaque,defense,competence,arme,armure,butin): 
+    def __init__(self,identite:str,race,classe,niv,pvmax,pv,attaque,defense,competence,arme,armure,butin): 
         super().__init__(identite,race,classe,niv,pvmax,pv,attaque,defense,competence,arme,armure)
         self.__butin=butin
-
-
-
 
     def get_butin(self):
         return self.__butin
