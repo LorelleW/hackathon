d08122354c5099a04a72fa40cb35f34522a299d9
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 16:07:18 +0200

    Update inventaire.py

--- classes/inventaire.py
+++ classes/inventaire.py
@@ -4,3 +4,28 @@ public class Inventaire():
         self.__armures=armures
         self.__armes=armes
         self.__objets=objets
+
+    def get_livres(self):
+        return self.__livres
+
+    def set_livres(self, livres):
+        self.__livres = livres
+
+    def get_armures(self):
+        return self.__armures
+
+    def set_armures(self, armures):
+        self.__armures = armures
+
+    def get_armes(self):
+        return self.__armes
+
+    def set_armes(self, armes):
+        self.__armes = armes
+
+    def get_objets(self):
+        return self.__objets
+
+    def set_objets(self, objets):
+        self.__objets = objets
+
