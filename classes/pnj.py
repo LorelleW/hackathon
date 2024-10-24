c3b96365fa5804c8d2992267a7cd1dfa0f2a6ed9
2ec3 <eric-shao@hotmail.com>
2024-10-24 14:05:47 +0200

    Create pnj.py

---
+++ classes/pnj.py
@@ -0,0 +1,7 @@
+class pnj():
+    def __init__(self,identite,metier:str,statistiques,inventaire,avisdujoueur): 
+        self.__identite=identite
+        self.__metier=metier
+        self.__statistiques=statistiques
+        self.__inventaire=inventaire
+        self.__avisdujoueur=avisdujoueur
