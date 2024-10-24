7988fec9563cac45c1f93a11a48c91324fc573c9
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 13:57:51 +0200

    Create race.py

---
+++ classes/race.py
@@ -0,0 +1,6 @@
+class race():
+    def __init__(self,nom:str,region:str,rarete,reputation):
+        self.__nom=nom 
+        self.__region=region 
+        self.__rarete=rarete #int? str?
+        self.__reputation=reputation #?
