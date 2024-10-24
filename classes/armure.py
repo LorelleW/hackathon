e1c33a6bb707b593543bb0964a1897aa51461636
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 14:05:54 +0200

    Create armure.py

---
+++ classes/armure.py
@@ -0,0 +1,7 @@
+class armure():
+    def __init__(self,type5:str,restriction,defense,rarete,etat:str): 
+        self.__type5=type5 
+        self.__restriction=restriction
+        self.__defense=defense
+        self.__rarete=rarete
+        self.__etat=etat
