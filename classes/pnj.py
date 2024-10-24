a946658e19ed92c2e5e922a3b24b7cc40a05b05c
Minetriforce <jonathan.ngo@etu.cyu.fr>
2024-10-24 15:20:52 +0200

    Update pnj.py

--- classes/pnj.py
+++ classes/pnj.py
@@ -5,3 +5,34 @@ public class Pnj():
         self.__statistiques=statistiques
         self.__inventaire=inventaire
         self.__avisdujoueur=avisdujoueur
+
+
+    def get_identite(self):
+        return self.__identite
+
+    def set_identite(self,identite):
+        self.__identite=identite
+
+    def get_metier(self):
+        return self.__metier
+
+    def set_metier(self,metier):
+        self.__metier=metier
+
+    def get_statistiques(self):
+        return self.__statistiques
+
+    def set_statistiques(self,statistiques):
+        self.__statistiques=statistiques
+
+    def get_inventaire(self):
+        return self.__inventaire
+
+    def set_inventaire(self,inventaire):
+        self.__inventaire=inventaire
+
+    def get_avisdujoueur(self):
+        return self.__avisdujoueur
+
+    def set_avisdujoueur(self,avisdujoueur):
+        self.__avisdujoueur=avisdujoueur
