f08054a015c69f2e78158831f44d38a1a1dbff98
2ec3 <eric-shao@hotmail.com>
2024-10-24 17:34:32 +0200

    Update armure.py

--- classes/armure.py
+++ classes/armure.py
@@ -1,4 +1,4 @@
-public class Armure():
+class Armure():
     def __init__(self,type5:str,restriction,defense,rarete,etat:str,prix:int): 
         self.__type5=type5 
         self.__restriction=restriction
