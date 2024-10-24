4b1a5b28295d4a2ac67c25c5fff05bf3f15903e5
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 14:06:42 +0200

    Create inventaire.py

---
+++ classes/inventaire.py
@@ -0,0 +1,6 @@
+class inventaire():
+    def __init__(self,livre,armure,arme,objet): #mettre un système % de chance
+        self.__livre=livre
+        self.__armure=armure
+        self.__arme=arme
+        self.__objet=objet
