c445ad01b287360c3f97234b26ca288a78aa29d0
Floriannee <floriannepan@gmail.com>
2024-10-24 14:58:28 +0200

    Update creature.py

--- classes/creature.py
+++ classes/creature.py
@@ -1,5 +1,5 @@
 public class Creature():
-    def __init__(self,nom:str,race:str,identite,classe,competences,attaque,niv,pv,butin): #butin creer nouvelle class? 
+    def __init__(self,nom:str,race:str,identite,classe,competences,attaque,niv,pv,arme,butin): #butin creer nouvelle class? 
         self.__nom=nom
         self.__race=race
         self.__classe=classe
@@ -7,4 +7,5 @@ public class Creature():
         self.__attaque=attaque
         self.pv=pv
         self.niv=niv
+        self.arme=arme
         self.__butin=butin
