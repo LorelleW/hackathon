3d64d02e3abb3d8026d938760c8d6fe4fe85bfea
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 15:20:04 +0200

    Update butin.py

--- classes/butin.py
+++ classes/butin.py
@@ -5,3 +5,38 @@ public class Butin():
         self.__arme=arme
         self.__objet=objet
         self.__argent=argent
+
+    def get_livre(self):
+        return self.__livre
+
+    def set_livre(self,livre):
+        self.__livre=livre
+
+    def get_armure(self):
+        return self.__armure
+
+    def set_armure(self,armure):
+        self.__armure=armure
+
+    def get_arme(self):
+        return self.__arme
+
+    def set_arme(self,arme):
+        self.__arme=arme
+
+    def get_objet(self):
+        return self.__objet
+
+    def set_objet(self,objet):
+        self.__objet=objet
+
+    def get_argent(self):
+        return self.__argent
+
+    def set_argent(self,argent):
+        self.__argent=argent
+        
+        
+        
+        
+        
