a69e080cba3a7e09253b5af7853a09fad5e5a207
LorelleW <lorelle.weng@icloud.com>
2024-10-24 15:20:36 +0200

    Update degat.py

--- fonctions/degat.py
+++ fonctions/degat.py
@@ -14,3 +14,19 @@ def degat_joueur():
 
 def degat_creature():
   return (attaque_creature+attaque_arme_creature)
+
+def pv_actuel_joueur():
+  pv_joueur=joueur.get_pv(self)
+  degat_creat=degat_creature()
+  pv_joueur -= degat_creat
+  if (pv_joueur>0):
+    return (pv_joueur)
+  else:
+    print("Tu es mort...")
+
+def pv_actuel_creature():
+  pv_creature=creature.get_pv(self)
+  degat_joueur=degat_joueur()
+  pv_creature -= degat_joueur
+  if (pv_creature<=0):
+    print("Vous avez vaincu le monstre")
