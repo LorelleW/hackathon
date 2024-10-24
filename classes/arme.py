45a0bd0baaa00ec823fa6f9ec514d1c7fe161412
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 14:05:32 +0200

    Create arme.py

---
+++ classes/arme.py
@@ -0,0 +1,7 @@
+class arme():
+    def __init__(self,type4:str,restriction,attaque,rarete,etat:str): 
+        self.__type4=type4
+        self.__restriction=restriction
+        self.__attaque=attaque
+        self.__rarete=rarete
+        self.__etat=etat
