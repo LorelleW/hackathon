83c6b398909201e5a65608ba3e0aec5830605f62
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 13:48:34 +0200

    Create competence.py

---
+++ competence.py
@@ -0,0 +1,6 @@
+class competence():
+    def __init__(self,nom:str,niveau:int,description:str,delai:int):
+        self.__nom=nom 
+        self.__niveau=niveau
+        self.__description=description
+        self.__delai=delai
