e44147240229c92fe8b77987cfa2333c4255ff58
2ec3 <eric-shao@hotmail.com>
2024-10-24 22:27:51 +0200

    Update creature.py

--- classes/creature.py
+++ classes/creature.py
@@ -1,4 +1,6 @@
-class Creature():
+from classes.combattant import Combattant
+
+class Creature(Combattant):
     def __init__(self,identite:str,race,classe,niv,pvmax,pv,attaque,defense,competence,arme,armure,butin): 
         super().__init__(identite,race,classe,niv,pvmax,pv,attaque,defense,competence,arme,armure)
         self.__butin=butin
