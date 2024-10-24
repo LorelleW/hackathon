dc45aac69fe52f790e3f8527e1d98e434f651617
LorelleW <lorelle.weng@icloud.com>
2024-10-24 19:07:22 +0200

    Update degat.py

--- fonctions/degat.py
+++ fonctions/degat.py
@@ -9,13 +9,16 @@ def degat_joueur():
   attaque_joueur=Joueur.get_attaque()
   arme_joueur = Joueur.get_arme()  # Pas besoin de passer 'joueur' ici
   attaque_arme_joueur = arme_joueur.get_attaque() #str nom arme 
-  return (attaque_joueur+attaque_arme_joueur)
+  return (attaque_joueur+attaque_arme_joueur )
 
 def degat_creature():
   attaque_creature=Creature.get_attaque()
   arme_creature = Creature.get_arme()  # Pas besoin de passer 'joueur' ici
   attaque_arme_creature=arme_creature.get_attaque()
-  return (attaque_creature+attaque_arme_creature)
+  defense_joueur=Joueur.get_defense()
+  armure_joueur=Joueur.get_armure()
+  defense_armure_joueur = armure_joueur.get_defense()
+  return (attaque_creature+attaque_arme_creature - (defense_joueur + defense_armure_joueur))
 
 def pv_actuel_joueur():
   pv_joueur=Joueur.get_pv()
