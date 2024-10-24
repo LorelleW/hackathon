dcfc4677e574c21d4918ca2484f3b73440e1f893
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 14:59:26 +0200

    Update armure.py

--- classes/armure.py
+++ classes/armure.py
@@ -5,3 +5,40 @@ public class Armure():
         self.__defense=defense
         self.__rarete=rarete
         self.__etat=etat
+        
+    def get_type5(self):
+        return self.__type5
+
+    def set_type5(self,type5):
+        self.__type5=type5
+
+    def get_restriction(self):
+        return self.__restriction
+
+    def set_restriction(self,restriction):
+        self.__restriction=restriction
+
+    def get_defense(self):
+        return self.__defense
+
+    def set_defense(self,defense):
+        self.__defense=defense
+
+    def get_rarete(self):
+        return self.__rarete
+
+    def set_rarete(self,rarete):
+        self.__rarete = rarete
+
+    def get_etat(self):
+         return self.__etat
+
+    def set_etat(self,etat):
+        self.__etat=etat
+        
+        
+        
+        
+    
+
+        
