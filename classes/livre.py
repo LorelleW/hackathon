bcbb999da22b501e99f8d9784ffec6ed2beb3f54
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 14:06:18 +0200

    Create livre.py

---
+++ classes/livre.py
@@ -0,0 +1,7 @@
+class livre():
+    def __init__(self,nom:str,type7:str,prix:int,effets:str,rarete): 
+        self.__nom=nom
+        self.__type7=type7
+        self.__prix=prix
+        self.__effets=effets
+        self.__rarete=rarete
