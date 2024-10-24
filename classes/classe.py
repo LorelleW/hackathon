4ca62e518361b3ef4d48351d8b64e3da8c2be0d5
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 15:25:09 +0200

    Update classe.py

--- classes/classe.py
+++ classes/classe.py
@@ -3,3 +3,25 @@ public class Classe():
         self.__nom=nom 
         self.__caracteristique=caracteristique
         self.__type2=type2
+
+    def get_nom(self):
+        return self.__nom
+
+    def set_nom(self,nom):
+        self.__nom=nom
+
+    def get_caracteristique(self):
+        return self.__caracteristique
+
+    def set_caracteristique(self,caracteristique):
+        self.__caracteristique=caracteristique
+
+    def get_type2(self):
+        return self.__type2
+
+    def set_type2(self,type2):
+        self.__type2=type2
+        
+        
+        
+        
