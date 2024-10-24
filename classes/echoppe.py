46e698bfd464a836af12551f4ad8631173ddea08
2ec3 <eric-shao@hotmail.com>
2024-10-24 14:05:04 +0200

    Create echoppe.py

---
+++ classes/echoppe.py
@@ -0,0 +1,7 @@
+class echoppe():
+    def __init__(self,inventaire,argent,batiment:str,type6:str,proprietaire:str): 
+        self.__inventaire=inventaire 
+        self.__argent=argent
+        self.__batiment=batiment
+        self.__type6=type6 
+        self.__proprietaire=proprietaire 
