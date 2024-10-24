71c85029c39dda52f50941ce45a64fcf23922688
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 14:01:47 +0200

    Create objet.py

---
+++ classes/objet.py
@@ -0,0 +1,7 @@
+class objet():
+    def __init__(self,nom:str,type3:str,prix:int,materiaux:str,rarete:str): 
+        self.__nom=nom 
+        self.__type3=type3
+        self.__prix=prix
+        self.__materiaux=materiaux
+        self.__rarete=rarete
