7c0bd931d6eb8dbb94ff507bc63a1bdf155ce1bc
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 14:23:14 +0200

    Update creature.py

--- classes/creature.py
+++ classes/creature.py
@@ -1,4 +1,4 @@
-public class creature():
+public class Creature():
     def __init__(self,nom:str,race:str,identite,classe,competences,statistiques,butin): #butin creer nouvelle class? 
         self.__nom=nom
         self.__race=race
