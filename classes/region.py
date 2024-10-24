b1b25accfe1f73f5e04ee46ed9b42a244ce42830
Minetriforce <jonathan.ngo@etu.cyu.fr>
2024-10-24 16:18:42 +0200

    Update region.py

--- classes/region.py
+++ classes/region.py
@@ -7,34 +7,34 @@ public class Region():
         self.__proprietaire=proprietaire
 
 #getter
-def get_typedelieu(self):
-    return self.__typedelieu
+    def get_typedelieu(self):
+        return self.__typedelieu
     
-def get_topographie(self):
-    return self.__topographie
+    def get_topographie(self):
+        return self.__topographie
 
-def get_population(self):
-    return self.__population
+    def get_population(self):
+        return self.__population
 
-def get_position(self):
-    return self.__position
+    def get_position(self):
+        return self.__position
 
-def get_proprietaire(self):
-    return self.__proprietaire
+    def get_proprietaire(self):
+        return self.__proprietaire
 
 #setter
-def set_typedelieu(self, lieu):
-    self.__weapon = lieu
+    def set_typedelieu(self, lieu):
+        self.__weapon = lieu
 
-def set_topographie(self,topographie):
-    self.__topographie=topographie
+    def set_topographie(self,topographie):
+        self.__topographie=topographie
 
-def set_population(self,population):
-    self.__population=population
+    def set_population(self,population):
+        self.__population=population
 
-def set_position(self,position):
-    return self.__position=position
+    def set_position(self,position):
+        return self.__position=position
 
-def set_proprietaire(self,proprietaire):
-    return self.__proprietaire=proprietaire
+    def set_proprietaire(self,proprietaire):
+        return self.__proprietaire=proprietaire
 
