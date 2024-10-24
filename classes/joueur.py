7c33a6b306d78e2de7de881e0222cbfb0826c467
2ec3 <eric-shao@hotmail.com>
2024-10-24 14:00:45 +0200

    Update joueur.py

--- classes/joueur.py
+++ classes/joueur.py
@@ -1,1 +1,16 @@
+class joueur():
+    def __init__(self,niv,pvmax,pv,attaque,defense,argent,identite,race,classe,inventaire,competences,arme,armure):
+        self.__niv=niv
+        self.__pvmax=pvmax
+        self.__pv=pv
+        self.__attaque=attaque
+        self.__defense=defense
+        self.__argent=argent
+        self.__identite=identite
+        self.__race=race
+        self.__classe=classe
+        self.__inventaire=inventaire
+        self.__competences=competences
+        self.__arme=arme
+        self.__armure=armure
 
