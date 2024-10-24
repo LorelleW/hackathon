32505a5918661093f318faa228c2c6089f4b84d6
Minetriforce <jonathan.ngo@etu.cyu.fr>
2024-10-24 15:15:58 +0200

    Update race.py

--- classes/race.py
+++ classes/race.py
@@ -4,18 +4,28 @@ public class Race():
         self.__region=region 
         self.__rarete=rarete #int? str?
         self.__reputation=reputation #?
-
-    def get_nom_race(self):
+#getter
+    def get_nom(self):
         return self.__nom
     
-    def get_region_race(self):
+    def get_region(self):
         return self.__region
 
-    def get_rarete_race(self):
+    def get_rarete(self):
         return self.__rarete
 
-    def get_position_race(self):
-        return self.__position
-
-    def get_reputation_race(self):
+    def get_reputation(self):
         return self.__reputation
+
+#setter
+    def set_nom(self, nom):
+        self.__nom = nom
+
+    def set_region(self, region):
+        self.__region = region
+
+    def set_rarete(self, rarete):
+        self.__rarete = rarete
+
+    def set_reputation(self, reputation):
+        self.__reputation = reputation
