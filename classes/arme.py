e1eb7735b611ec68b02e2264f2d240735f206044
PEmeyr <paul.school.rwa@gmail.com>
2024-10-24 14:47:02 +0200

    Update arme.py

--- classes/arme.py
+++ classes/arme.py
@@ -6,31 +6,31 @@ public class Arme():
         self.__rarete=rarete
         self.__etat=etat
 
-def guet_type4(self):
+def get_type4(self):
     return self.__type4
     
 def set_type4(self, type4):
         self.__nom = type4 
 
-def guet_restriction(self):
+def get_restriction(self):
     return self.__restriction
 
 def set_restriction(self, restriction):
     self.__restriction = restriction
 
-def guet_attaque(self):
+def get_attaque(self):
     return self.__attaque
 
 def set_attaque(self,attaque):
     self._attaque = attaque
 
-def guet_rarete(self):
+def get_rarete(self):
     return self.__rarete
 
 def set_rarete(self,rarete):
     self._rarete = rarete
 
-def guet_etat(self):
+def get_etat(self):
     return self.__etat
 
 def set_etat(self,etat):
