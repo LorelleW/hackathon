a849ecc0f8028f86ed447b829f95354bb764bf33
2ec3 <eric-shao@hotmail.com>
2024-10-24 14:13:55 +0200

    Update magie.py

--- classes/magie.py
+++ classes/magie.py
@@ -1,6 +1,7 @@
 public class magie():
-    def __init__(self,nom:str,description:str,statistiques,type1:str):
+    def __init__(self,nom:str,description:str,degat,cout,type1:str):
         self.__nom=nom 
         self.__description=description 
-        self.__statistiques=statistiques #à voir (?) 
+        self.__degat=degat
+        self.__cout=cout
         self.__type1=type1
