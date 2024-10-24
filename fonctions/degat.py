180acf2ef9d7a12bd22dc8b269847c2dbff41a67
LorelleW <lorelle.weng@icloud.com>
2024-10-24 15:31:19 +0200

    Update degat.py

--- fonctions/degat.py
+++ fonctions/degat.py
@@ -1,7 +1,7 @@
 
-from classes.arme import *
-from classes.joueur import *
-from classes.creature import *
+from classes.arme import Arme
+from classes.joueur import Joueur
+from classes.creature import Creature
 
 attaque_joueur=joueur.get_attaque(self)
 attaque_arme_joueur=arme.get_attaque(joueur.arme) #str nom arme 
@@ -19,6 +19,7 @@ def pv_actuel_joueur():
   pv_joueur=joueur.get_pv(self)
   degat_creat=degat_creature()
   pv_joueur -= degat_creat
+  joueur.set_pv(self,pv_joueur)
   if (pv_joueur>0):
     return (pv_joueur)
   else:
@@ -28,5 +29,6 @@ def pv_actuel_creature():
   pv_creature=creature.get_pv(self)
   degat_joueur=degat_joueur()
   pv_creature -= degat_joueur
+  creature.set_pv(self,pv_creature)
   if (pv_creature<=0):
     print("Vous avez vaincu le monstre")
