80e86e25a5777883802a5860c43d6b865bfa1be3
2ec3 <eric-shao@hotmail.com>
2024-10-24 14:05:25 +0200

    Create batiment.py

---
+++ classes/batiment.py
@@ -0,0 +1,7 @@
+class batiment():
+    def __init__(self,nom:str,age:int,architecture,proprietaire,context_h):
+        self.__nom=nom
+        self.__age=age
+        self.__architecture=architecture
+        self.__proprietaire=proprietaire
+        self.__context_h=context_h
