e1d5bee5a7e07a09c68faa2f9c72a416e4eff581
2ec3 <eric-shao@hotmail.com>
2024-10-24 16:31:45 +0200

    Update equiper.py

--- fonctions/equiper.py
+++ fonctions/equiper.py
@@ -2,22 +2,21 @@ from classes.joueur import Joueur
 from classes.inventaire import Inventaire
 
 def arme_in_inventaire(inventaire, arme):
-    armes = Inventaire.get_arme(self, inventaire)
+    armes = Inventaire.get_arme(inventaire)
     for a in armes:
         if (a == arme):
             return True
     return False
 
 def desequiper():
-    arme = Joueur.get_arme(self)
+    arme = Joueur.get_arme(joueur)
     if (arme != poing):
-        Inventaire.ajouter_arme(Joueur.get_inventaire(self))
-        Joueur.set_arme(self, poing)
+        Inventaire.ajouter_arme(Joueur.get_inventaire(joueur))
+        Joueur.set_arme(joueur, poing)
 
 def equiper(arme):
-    inventaire = Joueur.get_inventaire(self)
+    inventaire = Joueur.get_inventaire(joueur)
     if arme_in_inventaire(inventaire, arme):
         desequiper()
         Inventaire.supprimer_arme(inventaire, arme)
-        Joueur.set_arme(self, arme)
-
+        Joueur.set_arme(joueur, arme)
