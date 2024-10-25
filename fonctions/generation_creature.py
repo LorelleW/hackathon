931ded7e70e456d947b07c325671f9047cd498f5
Minetriforce <jonathan.ngo@etu.cyu.fr>
2024-10-25 15:28:35 +0200

    Update generation_creature.py

--- fonctions/generation_creature.py
+++ fonctions/generation_creature.py
@@ -15,7 +15,8 @@ def generer_slime(niv):
     slime.set_region(Lac_mort)
     nomade = Classe("nomade", "nu", "neutre")
     gelee = Objet("gelee", "autre", 2, "organique", "D")
-    butin_slime = Butin([], [], [], [gelee], 5)
+    armure_gluant = Armure("armure_gluant", 8, "C", 5, 100, 1, "torse")
+    butin_slime = Butin([], [armure_gluant], [], [gelee], 5)
     noms = ["slime rouge", "slime vert", "slime bleu", "slime jaune"]
 
     r = rd.randint(0, 3)
@@ -37,12 +38,13 @@ def generer_loup(niv):
     # Création des instances de base pour les loups
     Foret_sombre = Region("forêt", 10, {}, 20, "Chef de meute")
     loup = Race("loup", Foret_sombre, "C", 70)
-    Foret_sombre.set_population({loup: 10})
+    Foret_sombre.__dict__["population"] = {loup: 10}  # Assigner des loups à la forêt
     
     guerrier = Classe("guerrier", "armure légère", "agressif")
     fourrure = Objet("fourrure", "ressource", 15, "organique", "C")
     croc = Objet("croc de loup", "ressource", 5, "organique", "C")
-    butin_loup = Butin([], [], [], [fourrure,croc], 10)
+    epee_loup = Arme("epee_loup", 10, "B", 50, 100, 1, "epee")
+    butin_loup = Butin([], [], [epee_loup], [fourrure,croc], 10)
     noms = ["loup noir", "loup blanc", "loup bar"]
 
     # Génération de type de loup basé sur des probabilités spécifiques
@@ -73,7 +75,8 @@ def generer_dragon(niv):
     # Création des instances de base pour le dragon
     Montagne_dragon = Region("montagne", 50, {}, 100, "Ancien dragon")
     dragon_race = Race("dragon", Montagne_dragon, "S", 100)
-    Montagne_dragon.set_population({dragon_race: 1})
+    Montagne_dragon.__dict__["population"] = {dragon_race: 1}  # Associer le dragon à la région
+    
     dragon_classe = Classe("Ancien", "écailles résistantes", "agressif")
     ecaille = Objet("écaille de dragon", "ressource", 100, "organique", "S")
     dent = Objet("dent de dragon", "ressource", 150, "organique", "S")
